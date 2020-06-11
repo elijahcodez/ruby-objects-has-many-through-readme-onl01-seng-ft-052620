6e8f82ce7f9921315b4f01caa523cf57cf27c289
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-11 18:49:24 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -27,6 +27,17 @@ class Waiter
 
     best_tipped_meal.customer
   end
+  
+  jason = Waiter.new("Jason", 4)
+  lisa = Customer.new("Lisa", 24)
+  tim = Customer.new("Tim", 35)
+  terrance = Customer.new("Terrance", 27)
 
+  terrance.new_meal(jason, 50, 3)
+  lisa.new_meal(jason, 40, 10)
+  tim.new_meal(jason, 45, 8)
+
+  jason.best_tipper #=> #<Customer:0x00007f80829959a8 @name="Lisa", @age=24>
+  jason.best_tipper.name #=> "Lisa"
 
 end
